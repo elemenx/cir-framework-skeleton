bae27d913030c146266eec0ab416762b9347c354
dongdong <dongdong@elemenx.com>
2024-12-26 14:57:32 +0800

    fix: menu resource

--- src/Http/Resources/Menu/MenuResource.php
+++ src/Http/Resources/Menu/MenuResource.php
@@ -15,17 +15,18 @@ class MenuResource extends JsonResource
     public function toArray($request)
     {
         $data = [
-            'id'        => $this->id,
-            'title'     => $this->title,
-            'sub_title' => $this->sub_title,
-            'icon'      => $this->icon,
-            'path'      => $this->path,
-            'acl'       => $this->acl,
-            'query'     => $this->query,
-            'params'    => $this->params,
-            'sort'      => $this->sort,
-            'hidden'    => $this->hidden,
-            'parent_id' => is_null($this->parent_id) ? 0 : $this->parent_id,
+            'id'         => $this->id,
+            'title'      => $this->title,
+            'sub_title'  => $this->sub_title,
+            'icon'       => $this->icon,
+            'path'       => $this->path,
+            'acl'        => $this->acl,
+            'query'      => $this->query,
+            'params'     => $this->params,
+            'sort'       => $this->sort,
+            'hidden'     => $this->hidden,
+            'open_blank' => $this->open_blank,
+            'parent_id'  => is_null($this->parent_id) ? 0 : $this->parent_id,
         ];
 
         return $data;
